664e8eb6b7b909a669a00c64ccebeb25daeeb22e
Nikhil Vijayan <nkhilv@gmail.com>
2018-09-22 14:14:40 +0100

    added cases for lower case regex. Separated out the matching to another function

--- content.js
+++ content.js
@@ -3,18 +3,25 @@
 
 var elementsInsideBody = [...document.body.getElementsByTagName('*')];
 // This makes an array of everything inside the body tag
-//console.log(elementsInsideBody);
+
 
 //a function that loops through every single item
 function findAndReplace(){
   elementsInsideBody.forEach(element =>{
     element.childNodes.forEach(child =>{
       if(child.nodeType === 3){
-        child.nodeValue = child.nodeValue.replace(/brexit/gi, 'breadsticks');
+        replaceText(child);
       }
     });
 
   });
+}
+
+function replaceText (node) {
+  let value = node.nodeValue;
+  value = value.replace(/Brexit/gi, 'Breadsticks');
+  value = value.replace(/brexit/gi, 'breadsticks');
+  node.nodeValue = value;
 }
 
 window.onload = findAndReplace();
